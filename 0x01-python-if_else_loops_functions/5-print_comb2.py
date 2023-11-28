0670d1cdb9faf548a17eab5f7c7f61a792df6af4
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-11-28 12:20:03 -0800

    Small edit

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
-for num in range(100):
-    print(f"{num:02d}", end=", " if num < 99 else "\n")
+for i in range(0, 100):
+    if i < 99:
+        print("{:02d}, ".format(i), end='')
+    elif i == 99:
+        print("{:02d}".format(i))
