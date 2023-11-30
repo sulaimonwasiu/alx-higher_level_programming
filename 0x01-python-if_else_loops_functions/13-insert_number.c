9f19f0e232b7ccb2427f7d0b3b184d4f6a636d39
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-11-29 19:01:41 -0800

    Minor edit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,53 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * createNode - adds a new node at the end of a listint_t list
- * @data: integer to be included in new node
- * Return: address of the new element or NULL if it fails
+ * insert_node - prints all elements of a listint_t list
+ * @head: pointer to head of list
+ * @number: data
+ * Return: inserted node
  */
 
-listint_t* createNode(int data)
-		{
-    listint_t* newNode = (listint_t*)malloc(sizeof(listint_t));
-
-    if (newNode == NULL)
-    {
-        perror("Error creating a new node");
-        exit(EXIT_FAILURE);
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-    return (newNode);
-}
-
-/**
- * insert_node - inserts data in a sorted list
- * @head: pointer to pointer of first node of listint_t list
- * @number: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-
-
-listint_t* insert_node(listint_t** head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t* newNode = createNode(number);
+	listint_t *new = create_node(number);
 
-    if (*head == NULL || number < (*head)->data)
-    {
-        newNode->next = *head;
-        *head = newNode;
-        return (newNode);
-    }
+	if (*head == NULL || number < (*head)->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-    listint_t* current = *head;
+	listint_t *current = *head;
 
-    while (current->next != NULL && current->next->data < number)
-    {
-        current = current->next;
-    }
-
-    newNode->next = current->next;
-    current->next = newNode;
-
-    return (newNode);  
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
