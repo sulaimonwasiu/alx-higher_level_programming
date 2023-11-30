fe788f28accf348282e7db642929f8a1c83267be
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-11-30 07:24:47 -0800

    Importing functions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import calculator_1 as calc
+
+if __name__ == "__main__":
+
+    a = 10
+    b = 5
+    add = calc.add(a, b)
+    sub = calc.sub(a, b)
+    mul = calc.mul(a, b)
+    div = calc.div(a, b)
+    print("{} + {} = {}\n{} - {} = {}".format(a, b, add, a, b, sub))
+    print("{} * {} = {}\n{} / {} = {}".format(a, b, mul, a, b, div))
