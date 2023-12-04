ae1bd0dfa2dace9d3033a0bf74238cecb7fa6e22
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-12-03 18:58:36 -0800

    Maximum of a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,12 @@
 def max_integer(my_list=[]):
     if my_list is None or len(my_list) is 0:
         return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if max < my_list[i]:
-            max = my_list[i]
-    return max
+
+    if len(my_list) == 1:
+        return my_list[0]
+
+    biggest = my_list[0]
+    for i in range(1,len(my_list)):
+        if biggest < my_list[i]:
+            biggest = my_list[i]
+    return biggest
