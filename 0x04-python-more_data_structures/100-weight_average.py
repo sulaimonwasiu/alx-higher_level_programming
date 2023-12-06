3a552f9dd23a49064e4211f38059c8e58f60bde8
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-12-06 10:53:38 -0800

    Weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) is 0:
+    if len(my_list) == 0:
         return 0
     a = list(map(list, zip(*my_list)))
     b = [x * y for x, y in zip(a[0], a[1])]
