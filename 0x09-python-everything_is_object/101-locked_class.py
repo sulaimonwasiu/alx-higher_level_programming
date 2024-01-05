cb3e23d9251f8a6a4f670e0b7f3b4cc1899de1e2
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-05 18:42:19 +0300

    Edited locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,9 +5,10 @@ This module provides a simple Locked class.
 
 
 class LockedClass:
-    """A simple Locked class"""
+    """A simple locked class"""
     def __setattr__(self, name, value):
         if name != 'first_name':
-            msg = f"'LockedClass' object has no attribute '{name}'"
+            class_name = self.__class__.__name__
+            msg = f"'{class_name}' object has no attribute '{name}'"
             raise AttributeError(msg)
         self.__dict__[name] = value
