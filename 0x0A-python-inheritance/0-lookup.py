ddb9cbb655330b91e3f6404f11e2edb843a02165
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-08 23:26:33 +0300

    Get atributes and methods

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,6 +6,6 @@ Return list of available attr of objects
 
 def lookup(obj):
     """
-    return list of attr of objects
+    return list of attributes and methods of objects
     """
     return dir(obj)
