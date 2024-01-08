669dc6ec275c655616228e3ae3d1d126c0555560
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-09 01:17:56 +0300

    Implement public instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to write an empty class
+"""
+
+
+class BaseGeometry:
+    """Geometry class
+    """
+    def area(self):
+        """
+        check area"""
+        raise Exception("area() is not implemented")
