a2f598fd2d84b7cd3892441d88f2e2c31c5b0c8e
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-09 00:21:26 +0300

    Minor edit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Return list object in a sorted order
+"""
+
+
 class MyList(list):
+    """
+    Class inheriting the list class
+    """
     def print_sorted(self):
         print(sorted(self))
