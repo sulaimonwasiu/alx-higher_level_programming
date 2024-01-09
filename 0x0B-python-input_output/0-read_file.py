dc1b48a216230237abe961e119c518df5ed5c367
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-09 22:22:02 +0300

    Minor edit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,6 @@
 def read_file(filename=""):
     """Read file"""
     with open(filename, encoding='utf-8') as file:
-        print(file.read())
+        for line in file:
+            print(line, end="")
+
