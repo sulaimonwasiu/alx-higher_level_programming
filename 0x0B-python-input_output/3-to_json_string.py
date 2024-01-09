250408e7df5b6a54f5d9776f846dfdd7c8641071
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-09 23:24:25 +0300

    Convert to JSON

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Write a function that returns the JSON 
+Write a function that returns the JSON
 representation of an object (string)
 """
 
 
 import json
+
 
 def to_json_string(my_obj):
     """Convert to JSON"""
