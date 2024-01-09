3f149052cd7d302d6dd0a1e5edda2bcee698a51f
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-09 23:05:39 +0300

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@
 def append_write(filename="", text=""):
     """
     Write to a file
-    Append: if file exists 
+    Append: if file exists
     """
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text)
