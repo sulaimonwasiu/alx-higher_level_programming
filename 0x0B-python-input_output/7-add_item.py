09d7d9fdfbd8e8998a94894fd92f4a2645f3535b
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-10 00:29:20 +0300

    Add and save object to JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """Add item and save to JSON file"""
 import sys
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-data = load_from_json_file("add_item.json")
-obj = data + sys.argv[1:]
-save_to_json_file(obj, "add_item.json")
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
