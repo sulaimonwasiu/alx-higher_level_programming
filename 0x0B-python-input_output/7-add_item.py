cb2497b5b31046f6990f7d198f7175a5ce8b20d2
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-10 01:28:00 +0300

    Add, load, and save JSON

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists("add_item.json"):
     data = load_from_json_file("add_item.json")
-    obj = data + sys.argv[1:]
-    save_to_json_file(obj, "add_item.json")
+    data.extend(sys.argv[1:])
+    save_to_json_file(data, "add_item.json")
 else:
     if not os.path.exists("add_item.json"):
         save_to_json_file(sys.argv[1:], "add_item.json")
