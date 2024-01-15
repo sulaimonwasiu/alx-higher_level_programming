26b5da6594fd79374dc261074e5fa38b95112c48
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-16 00:54:26 +0300

    Save to file class method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
- 
+
     @classmethod
     def save_to_file(cls, list_objs):
         """JSON string representation of list_objs to a file"""
