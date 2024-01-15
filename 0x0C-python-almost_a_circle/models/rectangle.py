c3208859b026400330da0fbfb6f40b0a135fd2bf
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-16 00:00:09 +0300

    Display method added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,13 +74,12 @@ class Rectangle(Base):
     def area(self):
         """Area of Rectangle"""
         return self.__width * self.__height
-    """
+
     def display(self):
         for i in range(self.__y):
             print()
         for i in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
-    """
     """
     def __str__(self):
         msg = "[Rectangle] ({}) {}/{} - {}/{}"
