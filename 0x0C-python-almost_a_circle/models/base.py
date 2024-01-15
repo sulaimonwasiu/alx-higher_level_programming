65bd39e0cc4a24527682f9577dc66bb6a0c5f6a4
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-16 00:45:21 +0300

    JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,3 +18,11 @@ class Base:
     @classmethod
     def get_nb_objects(cls):
         return cls.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON string representation of list_dictionaries"""
+        import json
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
