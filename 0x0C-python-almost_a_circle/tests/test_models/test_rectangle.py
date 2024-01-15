07451a524dd5414fbd4c18716aaf7e12b8aa993a
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-01-16 02:24:11 +0300

    Tests added

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,7 +8,7 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """Test Rectangle class"""
     def test_1_id(self):
-        # Test if the id attribute is correctly assigned
+        """Test if the id attribute is correctly assigned"""
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.id, 1)
 
@@ -17,7 +17,6 @@ class TestRectangle(unittest.TestCase):
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
-
 
         self.assertRaises(ValueError, Rectangle, 0, 0)
 
@@ -28,7 +27,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r6.id, 5)
 
     def test_2_width(self):
-        # Test if the width attribute is correctly assigned
+        """Test if the width attribute is correctly assigned"""
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
 
@@ -37,13 +36,12 @@ class TestRectangle(unittest.TestCase):
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.width, 10)
-    
+
     def test_3_area(self):
-        # Test if the area method returns the correct area
+        """Test area method"""
         r4 = Rectangle(3, 2)
         self.assertEqual(r4.area(), 6)
 
-        
         r2 = Rectangle(2, 10)
         self.assertEqual(r2.area(), 20)
 
@@ -51,7 +49,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r3.area(), 56)
 
     def test_4_update(self):
-        # Test if the update method correctly assigns attributes
+        """Test update method"""
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(89)
         self.assertEqual(r1.id, 89)
@@ -69,10 +67,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.y, 5)
 
     def test_5_to_dictionary(self):
-        # Test if the to_dictionary method returns the correct dictionary
+        """Test to_dictionary method"""
         r1 = Rectangle(10, 2, 1, 9)
         r1_dictionary = r1.to_dictionary()
-        self.assertEqual(r1_dictionary, {'x': 1, 'y': 9, 'id': 11, 'height': 2, 'width': 10})
+        self.assertEqual(r1_dictionary,
+                         {'x': 1, 'y': 9, 'id': 11, 'height': 2, 'width': 10})
 
         r2 = Rectangle(1, 1)
         r2.update(**r1_dictionary)
