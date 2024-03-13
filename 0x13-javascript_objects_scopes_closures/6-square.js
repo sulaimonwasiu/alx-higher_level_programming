c683ed3b92ea2b02100c02f98fae5a29ad84341e
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-03-13 04:33:58 +0300

    Print square character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    
+    let str = c.repeat(this.size) + '\n';
+    str = str.repeat(this.size);
+    console.log(str);
+  }
+}
+
+module.exports = Square;
