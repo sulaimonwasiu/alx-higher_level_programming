7dfc6fbded17612d77750f557d266026e4549b0f
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-03-17 02:52:46 -0700

    .

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ def list_states(username, password, database):
                          db=database)
 
     cur = db.cursor()
-    cmd = """SELECT id, name
+    cmd = """SELECT *
              FROM states
              WHERE name LIKE 'N%'
              ORDER BY id ASC;"""
