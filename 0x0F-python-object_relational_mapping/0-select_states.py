099cc6aa18f08a41f9c533657202d429bffc8a7a
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-03-17 05:41:57 +0300

    Fixing style error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""List all states from database hbnt_0e_0_usa
+"""
 import MySQLdb
 import sys
 
 
 def list_states(username, password, database):
+    """List all states from database hbnt_0e_0_usa
+    """
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=username,
