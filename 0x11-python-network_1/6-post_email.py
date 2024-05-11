c49138b0f1fe6a301a5a2e5aa58248a62a3f4143
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-05-10 17:47:53 -0700

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,14 +5,14 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 
-import requests
-import sys
-
 
 if __name__ == "__main__":
+    import sys
+    import requests
+
     url = sys.argv[1]
     email = sys.argv[2]
 
     data = {'email': email}
-    response = requests.post(url, json=data)
+    response = requests.post(url, data)
     print(response.text)
