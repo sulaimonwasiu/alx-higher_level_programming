15eac06e862d368730a31a1e75a42be23dc0b3a2
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-06-04 15:13:53 -0700

    .

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
-#!/usr/bin/env node
-// read from file
+#!/usr/bin/node
+// Read from file
 
-const fs = require('fs');
-const filename = process.argv[2];
-
-if (filename === undefined) {
-} else {
-  fs.readFile(filename, 'utf8', function (err, data) {
-    if (err) console.log(err);
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
     console.log(data);
   });
-}
